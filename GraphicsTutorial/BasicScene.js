d21b5b5b91e81f519f7228bb6c77e98d910ea75a
BrieAnna.Allen@iam.local <BrieAnna.Allen@iam.local>
2018-11-19 09:13:05 -0600

    Add project files.

---
+++ GraphicsTutorial/BasicScene.js
@@ -0,0 +1,11 @@
+﻿var renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('myCanvas'), antialias: true });
+renderer.setClearColor(0x00ff00);
+renderer.setPixelRatio(window.devicePizelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+var camera = new THREE.PerespectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
+
+var scene = new THREE.Scene();
+
+renderer.render(scene, camera);
+//document.body.appendChild(renderer.domElement);
